3e08ba5f5e2bdd5c168c920ca4d0579c380202fb
TyGalea <galeaty@gmail.com>
2024-01-18 14:54:49 -0700

    Completed Week-2 Assignment

---
+++ app/week-2/student-info.js
@@ -0,0 +1,12 @@
+import Link from "next/link";
+
+export default function StudentInfo() {
+  return (
+    <>
+      <h1>Week 2</h1>
+      <Link href="https://github.com/TyGalea/cprg306-assignments">
+        https://github.com/TyGalea/cprg306-assignments
+      </Link>
+    </>
+  );
+}
