a48d762e7d93d60969c2afc036d1db9b66263fb6
TyGalea <galeaty@gmail.com>
2024-03-05 12:48:09 -0700

    week-7 fix
    modified the cleanup of ingredient

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -16,7 +16,7 @@ export default function Page() {
 
   const handleItemSelect = (name) => {
     name = name.replace(
-      /[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2580-\u27BF]|\uD83E[\uDD10-\uDDFF]| /g,
+      /[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2580-\u27BF]|\uD83E[\uDD10-\uDDFF]|/g,
       ""
     );
     name = name.split(",")[0];
